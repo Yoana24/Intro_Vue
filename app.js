510469ad96c2695654fff4ec34f1a99510eaacbb
Yoana24 <yoanapla@gmail.com>
2025-02-18 19:07:03 +0100

    setup and ref

--- app.js
+++ app.js
@@ -4,9 +4,28 @@ const { createApp, ref } = Vue;
 const app = createApp({
 
     template: `
-        <h1>Hola Mundo</h1>
+        <h1>{{ profesion }}</h1>
         <p>Desde app.js</p>
-    `
+    `, 
+
+    Setup() {
+
+        const profesion = ref('interiorista'); //el ref y value se pone cuando quieres que salga en unos segundos
+        const estudios = ref('bellasartes');
+
+        setTimeout(() => {
+            profesion.value = 'Modelador';
+            estudios.value = 'bellasartes';
+
+        } , 1000)
+
+        return { //esto es una funcion que devuelve un objeto, por los corchetes
+            profesion,
+            estudios,
+        }
+
+    }
+
 });
 
 app.mount('#myApp')
